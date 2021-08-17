74861e5978a756aee0870fc80c9e2832e880dac1
Bojan Malinic <bojan.malinic@outlook.com>
2021-08-17 23:45:45 +0200

    Initial commit

---
+++ modules/api/src/test/java/org/unibl/etf/youtubetrimmer/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.youtubetrimmer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
