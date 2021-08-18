bc3897b9896ae09d6d588f8c3ea7efe42bf9803a
Bojan Malinic <bojan.malinic@outlook.com>
2021-08-18 16:11:54 +0200

    Specify entity and repository scan packages

--- modules/api/src/main/java/org/unibl/etf/youtubetrimmer/api/ApiApplication.java
+++ modules/api/src/main/java/org/unibl/etf/youtubetrimmer/api/ApiApplication.java
@@ -2,8 +2,13 @@ package org.unibl.etf.youtubetrimmer.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("org.unibl.etf.youtubetrimmer.common.repository")
+@EntityScan("org.unibl.etf.youtubetrimmer.common.entity")
 public class ApiApplication {
 
     public static void main(String[] args) {
