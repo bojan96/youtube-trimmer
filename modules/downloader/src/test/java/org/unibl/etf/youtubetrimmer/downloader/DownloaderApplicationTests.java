ed549be8010acd17cf53a8041e0ac7ac0dda513b
Bojan Malinic <bojan.malinic@outlook.com>
2021-08-22 16:48:56 +0200

    Add downloader module

---
+++ modules/downloader/src/test/java/org/unibl/etf/youtubetrimmer/downloader/DownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.youtubetrimmer.downloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
