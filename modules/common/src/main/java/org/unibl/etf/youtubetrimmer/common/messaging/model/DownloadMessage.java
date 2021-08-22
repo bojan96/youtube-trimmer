65bda019003ec03b9909d203bb659d0822edbda5
Bojan Malinic <bojan.malinic@outlook.com>
2021-08-22 18:24:16 +0200

    Make DownloadMessage and TrimMessage serializable

--- modules/common/src/main/java/org/unibl/etf/youtubetrimmer/common/messaging/model/DownloadMessage.java
+++ modules/common/src/main/java/org/unibl/etf/youtubetrimmer/common/messaging/model/DownloadMessage.java
@@ -3,9 +3,12 @@ package org.unibl.etf.youtubetrimmer.common.messaging.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @Builder
-public class DownloadMessage {
+public class DownloadMessage implements Serializable {
+    private static final long serialVersionUID = 1;
     private int jobId;
     private String videoUrl;
 }
--- modules/common/src/main/java/org/unibl/etf/youtubetrimmer/common/messaging/model/TrimMessage.java
+++ modules/common/src/main/java/org/unibl/etf/youtubetrimmer/common/messaging/model/TrimMessage.java
@@ -3,7 +3,10 @@ package org.unibl.etf.youtubetrimmer.common.messaging.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @Builder
-public class TrimMessage {
+public class TrimMessage implements Serializable {
+    private static final long serialVersionUID = 1;
 }
