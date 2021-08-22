0fe59c8b01ad741e1bdc56df6a7e352d05d19636
Bojan Malinic <bojan.malinic@outlook.com>
2021-08-22 20:09:01 +0200

    Configure messaging for downloader

---
+++ modules/downloader/src/main/java/org/unibl/etf/youtubetrimmer/downloader/messaging/MessagingConfiguration.java
@@ -0,0 +1,30 @@
+package org.unibl.etf.youtubetrimmer.downloader.messaging;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.unibl.etf.youtubetrimmer.common.messaging.Queues;
+
+@Configuration
+public class MessagingConfiguration {
+
+    @Bean
+    public Queue downloadQueue() {
+        return new Queue(Queues.DOWNLOAD);
+    }
+
+    @Bean
+    public Queue trimQueue() {
+        return new Queue(Queues.TRIM);
+    }
+
+    @Bean("containerFactory")
+    public SimpleRabbitListenerContainerFactory containerFactory(ConnectionFactory connFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connFactory);
+        factory.setPrefetchCount(1);
+        return factory;
+    }
+}
