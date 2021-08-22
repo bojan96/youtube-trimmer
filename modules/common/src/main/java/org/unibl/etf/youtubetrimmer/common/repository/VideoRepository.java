a024e1efceff7b3d9a54496ba201f4c62e2cdbbc
Bojan Malinic <bojan.malinic@outlook.com>
2021-08-22 19:49:17 +0200

    Add missing Repository annotations

--- modules/common/src/main/java/org/unibl/etf/youtubetrimmer/common/repository/VideoRepository.java
+++ modules/common/src/main/java/org/unibl/etf/youtubetrimmer/common/repository/VideoRepository.java
@@ -1,7 +1,9 @@
 package org.unibl.etf.youtubetrimmer.common.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.unibl.etf.youtubetrimmer.common.entity.VideoEntity;
 
+@Repository
 public interface VideoRepository extends JpaRepository<VideoEntity, Integer> {
 }
