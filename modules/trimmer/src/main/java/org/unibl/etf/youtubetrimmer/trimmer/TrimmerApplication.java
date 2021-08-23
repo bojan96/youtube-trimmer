b30493b83d9141a2cc26e7b06f5108b542f9c5f5
Bojan Malinic <bojan.malinic@outlook.com>
2021-08-23 15:34:07 +0200

    Add trimmer module

---
+++ modules/trimmer/src/main/java/org/unibl/etf/youtubetrimmer/trimmer/TrimmerApplication.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.youtubetrimmer.trimmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrimmerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrimmerApplication.class, args);
+    }
+
+}
---
+++ modules/trimmer/src/test/java/org/unibl/etf/youtubetrimmer/trimmer/TrimmerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.youtubetrimmer.trimmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrimmerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
