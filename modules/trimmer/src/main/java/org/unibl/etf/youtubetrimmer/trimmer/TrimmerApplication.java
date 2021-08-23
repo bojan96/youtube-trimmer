b6d3ca2208aba476e89d921dc7fe8207e9d1a22c
Bojan Malinic <bojan.malinic@outlook.com>
2021-08-23 18:54:34 +0200

    Add mysql dependency, add necessary annotations for jpa

--- modules/trimmer/src/main/java/org/unibl/etf/youtubetrimmer/trimmer/TrimmerApplication.java
+++ modules/trimmer/src/main/java/org/unibl/etf/youtubetrimmer/trimmer/TrimmerApplication.java
@@ -2,8 +2,12 @@ package org.unibl.etf.youtubetrimmer.trimmer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("org.unibl.etf.youtubetrimmer.common.repository")
+@EntityScan("org.unibl.etf.youtubetrimmer.common.entity")
 public class TrimmerApplication {
 
     public static void main(String[] args) {
