27a2d09f9644ace1237c26c82839c2acba5496c8
Bojan Malinic <bojan.malinic@outlook.com>
2021-08-25 00:20:55 +0200

    Configure mapping between JobEventMessage and JobStatusMessage

--- modules/api/src/main/java/org/unibl/etf/youtubetrimmer/api/BeanConfig.java
+++ modules/api/src/main/java/org/unibl/etf/youtubetrimmer/api/BeanConfig.java
@@ -4,10 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.unibl.etf.youtubetrimmer.api.model.JobDetails;
+import org.unibl.etf.youtubetrimmer.api.model.message.JobStatusMessage;
 import org.unibl.etf.youtubetrimmer.api.util.LambdaConverter;
 import org.unibl.etf.youtubetrimmer.api.util.URLUtils;
 import org.unibl.etf.youtubetrimmer.common.entity.JobEntity;
 import org.unibl.etf.youtubetrimmer.common.entity.JobStatus;
+import org.unibl.etf.youtubetrimmer.common.messaging.model.JobEventMessage;
 
 @Configuration
 public class BeanConfig {
@@ -20,6 +22,11 @@ public class BeanConfig {
                         .map(JobEntity::getStatus, JobDetails::setStatus))
                 .addMappings(m -> m.using(LambdaConverter.of(URLUtils::getYoutubeUrl))
                         .map(j -> j.getVideo().getVideoUid(), JobDetails::setVideoUrl));
+
+        mapper.typeMap(JobEventMessage.class, JobStatusMessage.class)
+                .addMappings(m ->
+                        m.using(LambdaConverter.of(JobStatus::getDBValue))
+                        .map(JobEventMessage::getNewStatus, JobStatusMessage::setNewStatus));
         return mapper;
     }
 }
