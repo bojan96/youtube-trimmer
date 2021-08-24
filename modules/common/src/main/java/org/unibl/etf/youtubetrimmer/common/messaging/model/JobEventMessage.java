6a38254070b79383bf9b2cf98f4eed62b82b40d6
Bojan Malinic <bojan.malinic@outlook.com>
2021-08-25 00:16:16 +0200

    Add properties to JobEventMessage

--- modules/common/src/main/java/org/unibl/etf/youtubetrimmer/common/messaging/model/JobEventMessage.java
+++ modules/common/src/main/java/org/unibl/etf/youtubetrimmer/common/messaging/model/JobEventMessage.java
@@ -2,6 +2,7 @@ package org.unibl.etf.youtubetrimmer.common.messaging.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.unibl.etf.youtubetrimmer.common.entity.JobStatus;
 
 import java.io.Serializable;
 
@@ -9,4 +10,9 @@ import java.io.Serializable;
 @Builder
 public class JobEventMessage implements Serializable {
     private static final long serialVersionUID = 1;
+
+    private int id;
+    private String username;
+    private JobStatus newStatus;
+
 }
