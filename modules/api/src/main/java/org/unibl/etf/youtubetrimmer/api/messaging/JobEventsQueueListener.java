d9ba93e83843f2126e8280a86066c47e94e154da
Bojan Malinic <bojan.malinic@outlook.com>
2021-08-25 00:25:28 +0200

    Implement JobEventsQueueListener

---
+++ modules/api/src/main/java/org/unibl/etf/youtubetrimmer/api/messaging/JobEventsQueueListener.java
@@ -0,0 +1,27 @@
+package org.unibl.etf.youtubetrimmer.api.messaging;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+import org.unibl.etf.youtubetrimmer.api.model.message.JobStatusMessage;
+import org.unibl.etf.youtubetrimmer.common.messaging.Queues;
+import org.unibl.etf.youtubetrimmer.common.messaging.model.JobEventMessage;
+
+@Component
+@RequiredArgsConstructor
+@RabbitListener(queues = Queues.JOB_EVENT)
+public class JobEventsQueueListener {
+
+    private final SimpMessagingTemplate stompSender;
+    private final ModelMapper mapper;
+    private static final String JOB_STATUS_PATH = "/queue/job-events";
+
+    @RabbitHandler
+    public void handleMessage(JobEventMessage message) {
+        JobStatusMessage statusMessage = mapper.map(message, JobStatusMessage.class);
+        stompSender.convertAndSendToUser(message.getUsername(), JOB_STATUS_PATH, statusMessage);
+    }
+}
